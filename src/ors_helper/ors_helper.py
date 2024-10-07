c55e85312c966a66dba7165a6d5a8e294db63bc7
Benjamin Kupijai <kupijai@gmx.de>
2024-10-07 21:09:49 +0200

    [BK] 9 - added check_server_status to ors_helper.py

--- src/ors_helper/ors_helper.py
+++ src/ors_helper/ors_helper.py
@@ -3,6 +3,7 @@ This module sends requests to the openrouteservice server and generates
 and generates the final output
 """
 from typing import Union
+import requests
 import openrouteservice as ors
 
 class ORShelper:
@@ -11,4 +12,14 @@ class ORShelper:
     the data.
     """
     def __init__(self, server_url: str, api_key: Union[str, None]=None):
+        self.server_url=server_url
         self.client = ors.Client(base_url=server_url, key=api_key)
+        self.server_status = -1
+
+
+    def check_server_status(self) -> None:
+        """Sends health request to ORS server and sets server status to response.status_code"""
+
+        response = requests.get(f"{self.server_url}/ors/health")
+
+        self.server_status = response.status_code
--- tests/unit_tests/ors_helper/test_ors_helper.py
+++ tests/unit_tests/ors_helper/test_ors_helper.py
@@ -1,14 +1,35 @@
 """Tests for the ORShelper class"""
 
 import openrouteservice as ors
+import responses
 from src.ors_helper import ors_helper
 
 
 def test_ors_helper_init():
     """Tests if ORShelper gets correctly initialized"""
+    test_url = "http://127.0.0.1"
 
     test_instance = ors_helper.ORShelper(
-        server_url="http://127.0.0.1")
+        server_url=test_url)
 
     assert isinstance(test_instance, ors_helper.ORShelper)
     assert isinstance(test_instance.client, ors.Client)
+    assert test_instance.server_url == test_url
+
+@responses.activate
+def test_ors_helper_check_server_status_200():
+    """Checks if a request is made when server status gets checked"""
+    url = "http://some-ip"
+
+    responses.add(
+        method=responses.GET,
+        url=f"{url}/ors/health",
+        json={"key": "value"},
+        status=200
+    )
+
+    helper = ors_helper.ORShelper(server_url=url)
+
+    helper.check_server_status()
+
+    assert helper.server_status == 200
